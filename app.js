c14330b3ecf33f0e65fd56c03819264817440045
Andrew Gingrich <ajgingrich@yahoo.com>
2017-02-07 12:37:54 -0600

    add file upload form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var multer = require('multer');
 
 var app = express();
 
@@ -10,9 +11,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
-//use middleware
 
-//define routes
+
+
 //home
 app.get('/', function (req, res) {
     res.render('index')
@@ -23,14 +24,16 @@ app.get('/favicon.ico', function(req, res) {
     res.json(204);
 });
 
-var testy = {};
+var fileInfo = {};
 
-//get new URL, add to database and send
-app.get('/:id', function (req, res) {
-    testy.number1 = req.params.id;
-    res.json(testy)
+app.post('/', multer({dest: './uploads/'}).single('upload'), function(req, res) {
+
+    fileInfo.Bytes = req.file.size;
+    fileInfo.Kilobytes = req.file.size/1000;
+    fileInfo.Megabytes = req.file.size/1000000;
+    res.json(fileInfo);
+
 });
-
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
